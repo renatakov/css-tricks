4a08772a6d32596b7c454a1c072407626524ee15
renatakov <renatakov1010@gmail.com>
2024-06-16 18:04:09 +0200

    1

--- slider2.js
+++ slider2.js
@@ -6,33 +6,35 @@ const nextBtn = document.querySelector('#nextBtn')
 let index1 = 0,
     index2 = 1;
 
+function updateSliderImages() {
+    sliderImages[index1].style.display = 'block';
+    sliderImages[index2].style.display = 'block';
+}
+
 prevBtn.addEventListener('click', (e) => {
-    sliderImages[index1].style.display = 'none';
+    sliderImages[index1].style.transform = 'translateX(100%)';
     sliderImages[index2].style.display = 'none';
-    if(index1 === 0) {
-        sliderImages[index1].style.display = 'block';
-        sliderImages[index2].style.display = 'block';
+
+    // sliderImages[index1].style.display = 'none';
+    if (index1 === 0) {
+        updateSliderImages();
         return;
     }
     index1 = (index1 - 1 + sliderImages.length) % sliderImages.length
     index2 = index2 > 0 ? index2 - 1 : index1;
-    sliderImages[index1].style.display = 'block';
-    sliderImages[index2].style.display = 'block';
+    updateSliderImages();
 });
 
 nextBtn.addEventListener('click', (e) => {
-    sliderImages[index1].style.display = 'none';
+    sliderImages[index1].style.transform = 'translateX(-100%)';
+
     sliderImages[index2].style.display = 'none';
-    if(index2 === 5) {
-        sliderImages[index1].style.display = 'block';
-        sliderImages[index2].style.display = 'block';
+    if (index2 === 5) {
+        updateSliderImages();
         return;
     }
+    // sliderImages[index1].style.display = 'none';
     index1 = index2;
     index2 = (index2 + 1) % sliderImages.length;
-
-    sliderImages[index1].style.display = 'block';
-    sliderImages[index2].style.display = 'block';
+    updateSliderImages();
 });
-
-
